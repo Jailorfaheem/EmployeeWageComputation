fc6aaf5c2c6accb541087f8a4f746688a3bd5c8e
faheemjailor <faheemjailor0055>
2021-11-23 19:43:47 +0530

    faheems final solution for employee wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,10 +7,11 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             //Creating object of empWageBuilder class
-            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
             empWageBuilder.addCompanyEmpWage("Infosys", 30, 5, 60);
             empWageBuilder.addCompanyEmpWage("TCs", 25, 6, 70);
             empWageBuilder.computeEmpWage();
+            // Console.WriteLine("The total wage for Infosys company is :" + empWageBuilder.getTotalWage("Infosys"));
         }
     }
 }
