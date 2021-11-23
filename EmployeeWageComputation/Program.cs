80743248148b87b413212adf7a09dfee08c7bd34
faheemjailor <faheemjailor0055>
2021-11-23 19:38:35 +0530

    UC12 for emp

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,6 +6,7 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
+            //Creating object of empWageBuilder class
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
             empWageBuilder.addCompanyEmpWage("Infosys", 30, 5, 60);
             empWageBuilder.addCompanyEmpWage("TCs", 25, 6, 70);
