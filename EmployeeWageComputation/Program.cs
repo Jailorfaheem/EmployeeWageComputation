baa825a93a7fcf0438228e41201dfe80bed8c41d
faheemjailor <faheemjailor0055>
2021-11-23 19:13:35 +0530

    faheem UC3 daily wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,25 +7,37 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            //Constants
+            int Is_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-
+            //Variables
             int empHrs = 0;
             int empWage = 0;
-
+            //Random Object
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+
+            //if else condition for checking employee present or absent
+            if (empCheck == Is_FULL_TIME)
             {
+                Console.WriteLine("Employee type : Full Time");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee type : Part Time");
+                empHrs = 4;
             }
             else
             {
+                Console.WriteLine("Employee is absent");
                 empHrs = 0;
-
             }
+            //calculation of employee wage
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" + empWage);
+
+            Console.WriteLine("EMPLOYEE WAGE IS :" + empWage);
         }
     }
 }
