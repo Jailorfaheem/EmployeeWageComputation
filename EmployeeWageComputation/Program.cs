4fc3b467b3d78b0831f0216e6e6f6089ea2bad3d
faheemjailor <faheemjailor0055>
2021-11-23 19:21:04 +0530

    faheem UC3 for total monthly wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,29 +8,37 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-
-        static void Main(string[] args)
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
+            int totalempWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalempWage += empWage;
+                Console.WriteLine("Daily Employee Wage " + empWage);
+
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage :" + empWage);
+            Console.WriteLine("Total Monthly Employee Wage : " + totalempWage);
+
+
         }
     }
 }
